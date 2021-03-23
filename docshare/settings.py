00ab2671fda4be4e9e73c9762f50bff2fd62ed79
Swati Jaiswal <jaiswalswati94@gmail.com>
2021-03-23 12:56:26 +0000

    add rest framework to the project

--- docshare/settings.py
+++ docshare/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'rest_framework',
+
+    # Project apps
+    'api'
 ]
 
 MIDDLEWARE = [
